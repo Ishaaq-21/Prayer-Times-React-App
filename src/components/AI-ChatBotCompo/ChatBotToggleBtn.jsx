40d55c70e565055d8aaf80c9076db84a8b9969ef
Ishak <ishak.hadj14@gmail.com>
2025-07-02 18:39:29 +0100

    style: hide the toggle button when the chatbot is expanded

--- src/components/AI-ChatBotCompo/ChatBotToggleBtn.jsx
+++ src/components/AI-ChatBotCompo/ChatBotToggleBtn.jsx
@@ -8,18 +8,18 @@ export default function ChatBotToggleBtn({
       className={`fixed block lg:hidden bottom-6 ${activeLang === "en" ? "right-8 left-auto" : "left-8 right-auto"} z-10`}
     >
       <button
-        class={`      bg-gradient-to-r from-amber-500 to-orange-600 text-white p-3 rounded-full shadow-lg hover:scale-110 hover:from-amber-400 hover:to-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500 focus:ring-offset-gray-900 transition-transform duration-300 ease-in-out  ${expand ? "opacity-0 invisible" : "opacity-1 visible"}`}
+        className={`bg-gradient-to-r from-amber-500 to-orange-600 text-white p-3 rounded-full shadow-lg hover:scale-110 hover:from-amber-400 hover:to-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500 focus:ring-offset-gray-900 transition-transform duration-300 ease-in-out  ${expand ? "opacity-0 invisible hidden" : "opacity-1 visible block"}`}
         onClick={handleToggleClick}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
+          className="h-6 w-6"
           viewBox="0 0 24 24"
           fill="currentColor"
         >
           <path d="M21.99 4c0-1.1-.89-2-1.99-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18zM18 14H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z" />
         </svg>
-        <span class="sr-only">Open Chatbot</span>
+        <span className="sr-only">Open Chatbot</span>
       </button>
     </div>
   );

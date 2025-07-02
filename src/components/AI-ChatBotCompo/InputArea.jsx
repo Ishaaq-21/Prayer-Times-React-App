227c186f214965f001411d611c243856f44ae840
Ishak <ishak.hadj14@gmail.com>
2025-07-02 17:56:05 +0100

    feat: add auto focus on the input area when the chatbot is first opened

--- src/components/AI-ChatBotCompo/InputArea.jsx
+++ src/components/AI-ChatBotCompo/InputArea.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Icon } from "./ChatBot";
 
 export default function InputArea({ activeLang, handleClick, t }) {
   const [input, setInput] = useState("");
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  });
   return (
     <footer className={`p-4 border-t border-gray-700 flex-shrink-0`}>
       <div
@@ -13,11 +17,12 @@ export default function InputArea({ activeLang, handleClick, t }) {
           type="text"
           value={input}
           placeholder={t("askAiPlaceHolder")}
-          className="flex-1 bg-transparent text-gray-200 placeholder-gray-500 focus:outline-none px-2"
+          className={`flex-1 bg-transparent text-gray-200 text-[13px] placeholder-gray-500 focus:outline-none  px-2 ${activeLang === "en" ? "font-inter" : "font-cairo"}`}
           onChange={(e) => setInput(e.target.value)}
+          ref={inputRef}
         />
         <button
-          className={`p-2.5 rounded-full ${activeLang === "ar" ? "transform rotate-180" : ""}  bg-gradient-to-r from-orange-500 to-amber-500 text-white cursor-pointer`}
+          className={`p-2.5 rounded-full ${activeLang === "ar" ? "transform rotate-180" : ""}  bg-gradient-to-r from-orange-500 py-1 px-2 to-amber-500 text-white cursor-pointer`}
           onClick={() => {
             setInput("");
             handleClick(input);
@@ -26,8 +31,10 @@ export default function InputArea({ activeLang, handleClick, t }) {
           <Icon name="send" className="w-5 h-5" />
         </button>
       </div>
-      <p className="w-fit mx-auto text-xs text-accent-500 mt-1">
-        This AI can make mistakes, so double-check it{" "}
+      <p
+        className={`w-fit mx-auto text-[10px] text-accent-500 mt-2 -mb-[5px] text-center ${activeLang === "en" ? "font-inter" : "font-cairo"}`}
+      >
+        {t("aiNotice")}
       </p>
     </footer>
   );

8d2a2784cc0ca0ca00ad0779998866f0cd28d1f3
Ishak <ishak.hadj14@gmail.com>
2025-07-03 18:23:16 +0100

    fix : fix a typo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,13 +7,13 @@ export default function Header() {
 
   return (
     <div className="mt-3  w-full">
-      <LanguageToggle />
+      <LanguageToggle /> 
       <h1 className="title-shadow text-center  mb-2 max-w-[250px] mx-auto sm:max-w-none  text-4xl md:text-5xl 2xl:text-6xl text-secondary-500 text-shadow-lg  font-bold">
         {t("appTitle")}
       </h1>
       <blockquote
         className="text-center max-w-[70%] md:max-w-[60%] lg:min-h-[70px] mx-auto text-white text-[14px] font-bold mt-5 2xl:mt-8 text-shadow-lg
-"
+" 
       >
         <i>
           {t("hadith")}

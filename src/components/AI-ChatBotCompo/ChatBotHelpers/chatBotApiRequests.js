93474e2a6758d4dd80bff99a1154854089d6ea37
Ishak <ishak.hadj14@gmail.com>
2025-07-03 18:22:24 +0100

    add untracked files apiRequests module and typeLoading styling

---
+++ src/components/AI-ChatBotCompo/ChatBotHelpers/chatBotApiRequests.js
@@ -0,0 +1,22 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const geminiKey = import.meta.env.VITE_GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(geminiKey);
+export async function getResponseFromAI(userMsg) {
+  try {
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+
+    const intructions = `You are an AI Islamic adviser offering general spiritual guidance in a respectful, faith-centered tone.
+    - Use Islamic phrases (e.g., in shā’ Allāh, jazāk Allāhu khayran) moderately and naturally.
+    - Do not answer fatwa-related questions (e.g., “Can I...”, “Is it ḥalāl...”), and instead reply: "I cannot provide fatwa. Please consult a qualified imam or sheikh for this kind of question." 
+    -For other questions, cite reliable Islamic sources (Qur’ān, ḥadīth, or scholars) when applicable. 
+    -Keep responses concise, clear, uplifting, and rooted in Islamic etiquette.
+     If unsure, refer users to a qualified scholar.`;
+    const result = await model.generateContent(intructions + "\n\n" + userMsg);
+    const response = result.response;
+    console.log(response.text());
+    return response.text();
+  } catch (error) {
+    throw new Error(error);
+  }
+}
